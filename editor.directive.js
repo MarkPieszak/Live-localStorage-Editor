f0e615dd8f395f7192dd5e4a5c82b99be2301a49
Mark Pieszak <mpieszak84@gmail.com>
2016-02-26 16:52:40 -0500

    Updated directive

--- editor.directive.js
+++ editor.directive.js
@@ -1,109 +1,123 @@
 ;(function () {
 	'use strict';
-	
+
 	angular.module('editor-app' , [])
 		.directive('storageEditor', ['$interval', function ($interval) {
 			return {
 				scope : {},
-				templateUrl : 'editor.directive.html',
 				bindToController: true,
+
+				templateUrl : 'editor.directive.html',
 				controllerAs: 'vmEditor',
 				controller: editorController
 			};
-	
+
 			function editorController () {
-	
+
 				var vm = this;
-	
+
 				vm.storage_types = ['localStorage', 'sessionStorage'];
-	
-				// Sets the default
-				vm.type = vm.storage_types[1];
-	
-				setStorageType(vm.type);
-				intervalLoop();
-	
-				vm.__backup = vm.storage;
-	
-				vm.changeType = function () {
+				vm.type = vm.storage_types[1]; // Sets the default
+
+				vm.changeType = changeType;
+				vm.addItem = addItem;
+				vm.deleteItem = deleteItem;
+				vm.save = save;
+				vm.editing = editing;
+				vm.clear = clear;
+
+				activate();
+
+				////////////////////////////////////
+				function activate() {
+
 					setStorageType(vm.type);
+					vm.__backup = vm.storage;
+					intervalLoop();
+				}
+
+				////////////////////////////////////
+				// #region Internal Methods
+				////////////////////////////////////
+
+				function editing (val) {
+
 				};
-	
-				vm.addItem = function (item) {
-					vm.storage[item.key] = item.val;
-					vm.newItem = [];
-				};
-	
-				vm.deleteItem = function (key) {
+
+				function deleteItem (key) {
 					delete vm.storage[key];
 				};
-	
-				vm.save = function () {
-	
+
+				function save () {
+
 					var actual_storage = vm.type === 'localStorage'
 						? localStorage
 						: sessionStorage;
-	
+
 					console.log(vm.storage);
-	
+
 					for (var key in vm.storage) {
 						console.log(key);
 						console.log(vm.storage[key]);
 						actual_storage.setItem(key, vm.storage[key]);
 					}
-	
-	
+
+
 					alert('Saved!');
 				};
-	
-				vm.editing = function (val) {
-	
+
+				vm.backup = function () {
+					vm.__backup = vm.storage;
 				};
-	
-				vm.clear = function () {
+
+				vm.restore = function () {
+					vm.storage = vm.__backup;
+				};
+
+				function changeType () {
+					setStorageType(vm.type);
+				};
+
+				function addItem (item) {
+					vm.storage[item.key] = item.val;
+					vm.newItem = [];
+				};
+
+				function clear () {
 					var response = confirm('Are you sure?\n\nThis will Clear -ALL- data within ' + vm.type);
 					if (response) {
 						vm.storage.clear();
 						return;
 					}
 				};
-	
-				///////////////////////////
-				// BACKUP & RESTORE
-	
-				vm.backup = function () {
-					vm.__backup = vm.storage;
-				};
-	
-				vm.restore = function () {
-					vm.storage = vm.__backup;
-				};
-	
+
 				function intervalLoop () {
-	
+
 					var previous = JSON.stringify(vm.storage);
-	
+
 					$interval(function () {
 						/*
 						var current = JSON.stringify(vm.storage);
-	
+
 						if (current !== previous) {
 							console.log('different!');
 							previous = JSON.stringify(vm.storage);
 						}
 						*/
-	
+
 					}, 500);
 				}
-	
+
 				function setStorageType (type) {
 					vm.storage = type === 'localStorage'
 						? localStorage
 						: sessionStorage;
 				}
-	
+
+				// #endregion
+
 			}
-	
+
 		}]);
-		
+
 }());
